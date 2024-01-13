03b46959cba17ee397bfaf265efe11189a44b72d
Sergio <sergio.pm.developer@gmail.com>
2024-01-13 14:25:34 +0000

    feat: initialize sign up page and form

---
+++ pages/sign-up.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+const signUpData = ref({
+	email: '',
+	password: '',
+})
+
+const submitForm = () => {
+	// Sign up logic
+}
+</script>
+
+<template>
+	<!-- Sign up form -->
+	<form @submit.prevent="submitForm">
+		<div>
+			<label for="email">Email</label>
+			<input
+				v-model="signUpData.email"
+				id="email"
+				type="email"
+				placeholder="Your email..."
+			/>
+		</div>
+
+		<div>
+			<label for="message">Password</label>
+			<input
+				v-model="signUpData.password"
+				id="password"
+				type="password"
+				placeholder="Your password..."
+			/>
+		</div>
+
+		<div>
+			<button type="submit">Send</button>
+		</div>
+	</form>
+</template>
